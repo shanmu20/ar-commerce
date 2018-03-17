c1b5d069375ff5bfe9b106223b0c297febafd26f
shanmu <b.shanmu20@gmail.com>
2018-03-18 00:00:45 +0530

    camera

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,35 @@
-$(document).ready(function(){
-	setTimeout(function(){
-			var element = document.getElementsByClassName('a-canvas')[0];
-			var hammertime = new Hammer(element);
-			hammertime.get('pinch').set({ enable: true });
-			hammertime.on('pinch', function(ev) {
-				//var entity = $('.asset-entity');
-				var entity =  document.querySelectorAll('.asset-entity');
-				var entity_len = entity.length;
-				for (var i=0; i<entity_len; i++){
-					var scale_val =  entity[i].getAttribute('scale');
-					if (ev.type === 'pinch' && ev.additionalEvent == "pinchout") {
-						var scale_x = scale_val.x + scale_val.x/10;
-						var scale_y = scale_val.y + scale_val.y/10;
-						var scale_z = scale_val.z + scale_val.z/10;
-						var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
-						
-					}else if(ev.type === 'pinch'  && ev.additionalEvent == "pinchin"){
-						if(scale_val.x>=0){
-							var scale_x = scale_val.x - scale_val.x/10;
-							var scale_y = scale_val.y - scale_val.y/10;
-							var scale_z = scale_val.z - scale_val.z/10;
-							var final_val = scale_x + ' ' + scale_y + ' ' + scale_z;
-						}
-					}
-					entity[i].setAttribute('scale',final_val);
-				}
-			});
-	},1000);
-});
+           var _this = this
+            var domElement = document.createElement('video');
+            domElement.setAttribute('autoplay', '');
+            domElement.setAttribute('muted', '');
+            domElement.setAttribute('playsinline', '');
+            domElement.style.width = '1280px'
+            domElement.style.height = '720px'
+            if (navigator.mediaDevices === undefined 
+                    || navigator.mediaDevices.enumerateDevices === undefined 
+                    || navigator.mediaDevices.getUserMedia === undefined  ){
+                alert("WebRTC issue! navigator.mediaDevices.enumerateDevices not present in your browser");		
+            }
+            navigator.mediaDevices.enumerateDevices().then(function(devices) {
+                        var userMediaConstraints = {
+                    audio: false,
+                    video: {
+                        facingMode: 'environment',
+                       width: 1280, height: 720
+                    }
+                        }
+                navigator.mediaDevices.getUserMedia(userMediaConstraints).then(function success(stream) {
+                    // set the .src of the domElement
+                    domElement.srcObject = stream;
+                    // to start the video, when it is possible to start it only on userevent. like in android
+                    document.body.addEventListener('click', function(){
+                        domElement.play();
+                    })
+                    document.body.appendChild(domElement);
+                }).catch(function(error) {
+                    console.log("Can't access user media", error);
+                    alert("Can't access user media :()");
+                });
+            }).catch(function(err) {
+                console.log(err.name + ": " + err.message);
+            });
